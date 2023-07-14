681119caec1ff71cae7650d1a0b8beec5d91babe
unknown <auguste.delam@yahoo.fr>
2023-07-14 18:56:19 +0200

    Scène epee+four

--- Scripts/SDF/movement.cs
+++ Scripts/SDF/movement.cs
@@ -1,57 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static Shape;
-
-public class movement : MonoBehaviour
+using UnityEngine.UI;
+ 
+public class FpsCounter : MonoBehaviour
 {
-    // Start is called before the first frame update
+     
    
-    bool show = false;
-
-    void Start()
-    {
-        
-    }
-    
-    void Update()
-    {     
-        if(Input.GetKey(KeyCode.Return)){
-            if (! show) {
-                GetComponent<Shape>().operation = Operation.None;
-                show = true;
-            }
-            else {
-                GetComponent<Shape>().operation = Operation.Show;
-                show = false;
-            }
-                
-        }              
-        
-        if(Input.GetKey("o")){
-            transform.position = transform.position + new Vector3(1,0,0)*2*Time.deltaTime;
-        }
-
-        if(Input.GetKey("l")){
-            transform.position = transform.position - new Vector3(1,0,0)*2*Time.deltaTime;
-        }
-
-        if(Input.GetKey("k")){
-            transform.position = transform.position + new Vector3(0,0,1)*2*Time.deltaTime;
-        }
-
-        if(Input.GetKey(";")){
-            transform.position = transform.position - new Vector3(0,0,1)*2*Time.deltaTime;
-        }
-
-        if(Input.GetKey("i")){
-            transform.position = transform.position + new Vector3(0,1,0)*2*Time.deltaTime;
-        }
-
-        if(Input.GetKey("p")){
-            transform.position = transform.position - new Vector3(0,1,0)*2*Time.deltaTime;
-        }
-        
-    }
-
 }
