8ba4a2ef7a204db0a8664c556e8092f05a68a67f
David Louapre <david.louapre@gmail.com>
2023-07-14 19:36:47 +0200

    Script mouvement épée

--- Scripts/SDF/ObjectMove.cs
+++ Scripts/SDF/ObjectMove.cs
@@ -1,21 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectMove : MonoBehaviour
 {
-    
+    private Vector3 initialPosition;
+    private float time;
+    private const float AMPLITUDE = 4;
+    private const float FREQUENCY = 0.2f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
+        time = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        Vector3 new_pos = transform.position;
+        new_pos.z = initialPosition.z + 0.5f * AMPLITUDE * (1 + Mathf.Sin(-0.5f * Mathf.PI + FREQUENCY * time));
+        transform.position = new_pos;
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        
+    {
         if(Input.GetKey("w")){
             transform.position = transform.position + new Vector3(1,0,0)*2*Time.deltaTime;
         }
